18d0093a0cbfe3b1134132c5690b7db0c8e70512
Xenia Pietrzyk <xeniapietrzyk@gmail.com>
2022-01-25 23:25:37 +0100

    -add-AthleteConfiguration

---
+++ MVC/Db/Configuration/AthleteConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVC.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Db.SQL.Configuration
+{
+    public class AthleteConfiguration : IEntityTypeConfiguration<Athlete>
+    {
+        public static List<Athlete> collection = new List<Athlete>()
+        {
+            new Athlete
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Ewelina",
+                LastName = "Skoczna",
+                eMail = "ewelinaskoczna@athlead.com",
+                Type = MVC.Helpers.EmployeeType.athlete
+            },
+            new Athlete
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Dawid",
+                LastName = "Bicepsik",
+                eMail = "dawidbicepsik@athlead.com",
+                Type = MVC.Helpers.EmployeeType.athlete
+            }
+        };
+
+        public void Configure(EntityTypeBuilder<Athlete> builder)
+        {
+            builder.ToTable("Athlete");
+
+            //zawodnik ma jednego trenera ale trener ma wielu zawodnikow
+            builder
+                .HasOne(a => a.Trainer)
+                .WithMany(t=>t.Athletes);
+            
+            builder.HasData(collection);
+        }
+    }
+}
